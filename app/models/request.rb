235eadfe60d3948a41dd720f3b8a863812cf4f9d
Trinh Duc Duy <trinh.duc.duy@framgia.com>
2015-08-11 06:55:12 +0700

    Finish manage requests

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,7 +1,9 @@
 class Request < ActiveRecord::Base
   belongs_to :user
 
-  enum status: [:pending, :cancel, :resolved]
+  enum status: [:pending, :cancel, :rejected, :resolved]
 
   validates :book_title, presence: true
+
+  scope :latest, ->{order created_at: :desc}
 end
